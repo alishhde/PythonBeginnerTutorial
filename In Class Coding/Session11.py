31862fff974b8c877463fa68f9073aaea6ec2978
Seyyed Ali shohadaalhosseini <Al.shohadaee@gmail.com>
2022-02-05 18:25:43 +0330

    update

---
+++ In Class Coding/Session11.py
@@ -0,0 +1,30 @@
+from time import sleep
+ 
+# import turtle
+# x = 1
+# y = 1
+
+# z = 2
+# w = 2
+
+# def sum(a, b, pi=3.14):
+#     s = (a + b) * pi
+#     return s
+
+# print(sum(x, y, pi=3.14))
+
+
+# print("ali", "ali2", "ali3", sep="===>", end=" ")
+# print("ali5", end=" ")
+# print("ali6")
+
+# print("this is\n a\n test")
+
+
+for i in range(5):
+    print(i, end="\n", flush=True)
+    sleep(1)
+    
+    
+    
+((z - 2) * 180)/z
